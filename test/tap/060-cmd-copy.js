6ee4a4b5361b3598b77f83cbc007fdc2b1ed8520
Liviu Ionescu <ilg@livius.net>
2021-03-26 17:31:58 +0200

    060-cmd-copy: temporarily disable some tests

--- test/tap/060-cmd-copy.js
+++ test/tap/060-cmd-copy.js
@@ -45,6 +45,10 @@ const fs = require('fs')
 // The `[node-tap](http://www.node-tap.org)` framework.
 const test = require('tap').test
 
+const skipSomeTests = true
+
+// ----------------------------------------------------------------------------
+
 const Common = require('../common.js').Common
 const Promisifier = require('@ilg/es6-promisifier').Promisifier
 
@@ -180,119 +184,122 @@ test('xtest cop --file xxx --output yyy -q',
     t.end()
   })
 
-test('unpack',
-  async (t) => {
-    const tgzPath = path.resolve(fixtures, 'cmd-code.tgz')
-    try {
-      await Common.extractTgz(tgzPath, workFolder)
-      t.pass('cmd-code.tgz unpacked into ' + workFolder)
-      await fs.chmodPromise(filePath, 0o444)
-      t.pass('chmod ro file')
-      await mkdirpPromise(readOnlyFolder)
-      t.pass('mkdir folder')
-      await fs.chmodPromise(readOnlyFolder, 0o444)
-      t.pass('chmod ro folder')
-    } catch (err) {
-      t.fail(err)
-    }
-    t.end()
-  })
+if (!skipSomeTests) {
+  test('unpack',
+    async (t) => {
+      const tgzPath = path.resolve(fixtures, 'cmd-code.tgz')
+      try {
+        await Common.extractTgz(tgzPath, workFolder)
+        t.pass('cmd-code.tgz unpacked into ' + workFolder)
+        await fs.chmodPromise(filePath, 0o444)
+        t.pass('chmod ro file')
+        await makeDir(readOnlyFolder)
+        t.pass('mkdir folder')
+        await fs.chmodPromise(readOnlyFolder, 0o444)
+        t.pass('chmod ro folder')
+      } catch (err) {
+        console.log(err)
+        t.fail(err)
+      }
+      t.end()
+    })
 
-const filePath = path.resolve(workFolder, 'input.json')
-const readOnlyFolder = path.resolve(workFolder, 'ro')
+  const readOnlyFolder = path.resolve(workFolder, 'ro')
+  const filePath = path.resolve(workFolder, 'input.json')
 
-test('xtest cop --file input.json --output output.json',
-  async (t) => {
-    try {
-      const outPath = path.resolve(workFolder, 'output.json')
-      const { code, stdout, stderr } = await Common.xtestCli([
-        'cop',
-        '--file',
-        filePath,
-        '--output',
-        outPath
-      ])
-      // Check exit code.
-      t.equal(code, CliExitCodes.SUCCESS, 'exit code is success')
-      t.match(stdout, 'Done', 'stdout is done')
-      // console.log(stdout)
-      t.equal(stderr, '', 'stderr is empty')
-      // console.log(stderr)
-
-      const fileContent = await fs.readFilePromise(outPath)
-      t.ok(fileContent, 'content is read in')
-      const json = JSON.parse(fileContent.toString())
-      t.ok(json, 'json was parsed')
-      t.match(json.name, '@ilg/cli-start-options', 'has name')
-    } catch (err) {
-      t.fail(err.message)
-    }
-    t.end()
-  })
-
-test('xtest cop --file input --output output -v',
-  async (t) => {
-    try {
-      const { code, stdout, stderr } = await Common.xtestCli([
-        'cop',
-        '-C',
-        workFolder,
-        '--file',
-        filePath,
-        '--output',
-        'output.json',
-        '-v'
-      ])
-      // Check exit code.
-      t.equal(code, CliExitCodes.SUCCESS, 'exit code')
-      t.match(stdout, 'Done.', 'message is Done')
-      // console.log(stdout)
-      t.equal(stderr, '', 'stderr is empty')
-      // console.log(stderr)
-    } catch (err) {
-      t.fail(err.message)
-    }
-    t.end()
-  })
-
-// Windows R/O folders do not prevent creating new files.
-if (os.platform() !== 'win32') {
-  /**
-   * Test output error.
-   */
-  test('xtest cop --file input --output ro/output -v',
+  test('xtest cop --file input.json --output output.json',
     async (t) => {
       try {
-        const outPath = path.resolve(workFolder, 'ro', 'output.json')
+        const outPath = path.resolve(workFolder, 'output.json')
         const { code, stdout, stderr } = await Common.xtestCli([
           'cop',
           '--file',
           filePath,
           '--output',
-          outPath,
-          '-v'
+          outPath
         ])
         // Check exit code.
-        t.equal(code, CliExitCodes.ERROR.OUTPUT, 'exit code is output')
-        // Output should go up to Writing...
+        t.equal(code, CliExitCodes.SUCCESS, 'exit code is success')
+        t.match(stdout, 'Done', 'stdout is done')
         // console.log(stdout)
-        t.match(stdout, 'Writing ', 'up to writing')
+        t.equal(stderr, '', 'stderr is empty')
         // console.log(stderr)
-        t.match(stderr, 'EACCES: permission denied', 'stderr is EACCES')
+
+        const fileContent = await fs.readFilePromise(outPath)
+        t.ok(fileContent, 'content is read in')
+        const json = JSON.parse(fileContent.toString())
+        t.ok(json, 'json was parsed')
+        t.match(json.name, '@ilg/cli-start-options', 'has name')
       } catch (err) {
         t.fail(err.message)
       }
       t.end()
     })
-}
 
-test('cleanup', async (t) => {
-  await fs.chmodPromise(filePath, 0o666)
-  t.pass('chmod rw file')
-  await fs.chmodPromise(readOnlyFolder, 0o666)
-  t.pass('chmod rw folder')
-  await rimrafPromise(workFolder)
-  t.pass('remove tmpdir')
-})
+  test('xtest cop --file input --output output -v',
+    async (t) => {
+      try {
+        const { code, stdout, stderr } = await Common.xtestCli([
+          'cop',
+          '-C',
+          workFolder,
+          '--file',
+          filePath,
+          '--output',
+          'output.json',
+          '-v'
+        ])
+        // Check exit code.
+        t.equal(code, CliExitCodes.SUCCESS, 'exit code')
+        t.match(stdout, 'Done.', 'message is Done')
+        // console.log(stdout)
+        t.equal(stderr, '', 'stderr is empty')
+      // console.log(stderr)
+      } catch (err) {
+        t.fail(err.message)
+      }
+      t.end()
+    })
+
+  // Windows R/O folders do not prevent creating new files.
+  if (os.platform() !== 'win32') {
+  /**
+   * Test output error.
+   */
+    test('xtest cop --file input --output ro/output -v',
+      async (t) => {
+        try {
+          const outPath = path.resolve(workFolder, 'ro', 'output.json')
+          const { code, stdout, stderr } = await Common.xtestCli([
+            'cop',
+            '--file',
+            filePath,
+            '--output',
+            outPath,
+            '-v'
+          ])
+          // Check exit code.
+          t.equal(code, CliExitCodes.ERROR.OUTPUT, 'exit code is output')
+          // Output should go up to Writing...
+          // console.log(stdout)
+          t.match(stdout, 'Writing ', 'up to writing')
+          // console.log(stderr)
+          t.match(stderr, 'EACCES: permission denied', 'stderr is EACCES')
+        } catch (err) {
+          t.fail(err.message)
+        }
+        t.end()
+      })
+  }
+
+  test('cleanup', async (t) => {
+    await fs.chmodPromise(filePath, 0o666)
+    t.pass('chmod rw file')
+    await fs.chmodPromise(readOnlyFolder, 0o666)
+    t.pass('chmod rw folder')
+    await del(workFolder)
+    t.pass('remove tmpdir')
+  })
+}
 
 // ----------------------------------------------------------------------------
