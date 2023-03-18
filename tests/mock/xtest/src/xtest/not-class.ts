21056ac9747559b01c2ca9e669b3a73e811388b1
Liviu Ionescu <ilg@livius.net>
2023-03-18 12:35:54 +0200

    'use strict'

--- tests/mock/xtest/src/xtest/not-class.ts
+++ tests/mock/xtest/src/xtest/not-class.ts
@@ -9,7 +9,6 @@
  * be obtained from https://opensource.org/licenses/mit/.
  */
 
-'use strict'
 /* eslint max-len: [ "error", 80, { "ignoreUrls": true } ] */
 
 // ----------------------------------------------------------------------------
