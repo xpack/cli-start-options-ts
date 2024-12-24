39f5b9fb48f64549b61447143333684964f66e20
Liviu Ionescu <ilg@livius.net>
2024-12-24 14:54:33 +0200

    samples/01-minimal

---
+++ samples/01-minimal/src/main.ts
@@ -0,0 +1,59 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2023 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/mit/.
+ */
+
+/* eslint max-len: [ "error", 80, { "ignoreUrls": true } ] */
+
+// ----------------------------------------------------------------------------
+
+import * as path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+// ----------------------------------------------------------------------------
+
+import * as cli from '../../../esm/index.js'
+
+// ============================================================================
+
+export class Test extends cli.Application {
+  // --------------------------------------------------------------------------
+
+  constructor (params: cli.ApplicationConstructorParams) {
+    super(params)
+
+    const context: cli.Context = this.context
+    const log = context.log
+    log.trace(`${this.constructor.name}.constructor()`)
+
+    // Mandatory, must be set here, not in the library, since it computes
+    // the root path as relative to the path of this file..
+    context.rootPath =
+      path.dirname(path.dirname(fileURLToPath(import.meta.url)))
+  }
+
+  override async main (
+    argv: string[]
+  ): Promise<number> {
+    const context: cli.Context = this.context
+
+    const log = context.log
+    log.always('Hello World!')
+
+    argv.forEach((arg, ix) => {
+      log.info(`argv[${ix}]='${arg}'`)
+    })
+
+    return cli.ExitCodes.SUCCESS
+  }
+
+  // --------------------------------------------------------------------------
+}
+
+// ----------------------------------------------------------------------------
